6ae44ff538448788b2a89bd0c1fd640b676ee29e
gciotto <gustavociotto@gmail.com>
2016-03-20 19:50:44 -0300

    CV

--- Learning Python - Part I/exercise2.py
+++ Learning Python - Part I/exercise2.py
@@ -6,3 +6,7 @@ print(string + '2', type (string), int(string) + 2)
 if type(int(string)) != str:
     print('coco') 
 
+I = [[[ 1, 2, 3 ], [4, 5, 6]] ,[[ 1, 2, 3 ], [4, 5, 6]], [[ 1, 2, 3 ], [4, 5, 6]]]
+print(I[0], I[1], I[2])
+
+print ([[(x+y+z)/3 for x in I[0] for y in I[1] for z in I[2]]])
