68f445811e122065d24a7639aaf3880f21286b5b
gciotto <gustavociotto@gmail.com>
2016-07-06 15:10:12 -0300

    delete useless projects

--- RemoteSystemsTempFiles/192.168.2.12/root/gciotto/PRU_timer/overlay.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /lib/firmware/
-echo pru_overlay > $SLOTS
-cd ~/gciotto/PRU_timer/
-
--- RemoteSystemsTempFiles/192.168.7.2/root/gciotto/PRU_timer_server/init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-pasm -b pru_timer_server.p
-gcc pru_ethernet_server_loader.c -o pru_ethernet_server_loader -lprussdrv
