1d6a5a81de98baa183b3b3bf1f5a41b912e3fc87
Gustavo Ciotto Pinton <gustavo.pinton@lnls.br>
2016-08-04 13:53:10 -0300

    node list updated

--- CNPEM - PROSAC Daemon Client/Control_Node.py
+++ CNPEM - PROSAC Daemon Client/Control_Node.py
@@ -94,43 +94,43 @@ class Network_Nodes():
 
     nodes = []
 
-    #### TESTES
-    nodes.append(Control_Node(47, 5, "r47n5", "10.128.47.5"))
-    nodes.append(Control_Node(47, 6, "r47n6", "10.128.47.6"))
+#     #### TESTES
+#     nodes.append(Control_Node(47, 5, "r47n5", "10.128.47.5"))
+#     nodes.append(Control_Node(47, 6, "r47n6", "10.128.47.6"))
      
-#     #redes 36 e 38
-#     nodes.append(Control_Node(36,1,'r36n1','10.128.1.127'))
-#     nodes.append(Control_Node(38,2,'r38n2','10.128.1.120'))
-#     
-#     # rede 45
-#     # 1 a 14
-#     nodes.extend([Control_Node(45,i,'r45n%d'%i,'10.128.45.%d'%i) for i in range(1,15)])
-#     
-#     #rede 46
-#     # 1 a 9, impares
-#     nodes.extend([Control_Node(46,i,'r46n%d'%i,'10.128.46.%d'%i) for i in range(1,10,2)])
-#     # 10 a 14
-#     nodes.extend([Control_Node(46,i,'r46n%d'%i,'10.128.46.%d'%i) for i in range(10,15)])
-#     
-#     #nodes.append(Control_Node(45,9,'r45n9','10.128.45.9'))
-#     #nodes.append(Control_Node(45,10,'r45n10','10.128.45.10'))
-#     #nodes.append(Control_Node(45,11,'r45n11','10.128.45.11'))
-#     #uvx_network.append(Control_Node(45,11,'r45n11','10.128.45.11'))
-#     
-#     # Adicionado em 21/03/2016 (cavidades de RF do anel)
-#     nodes.append(Control_Node(48, 1, "r48n1", "10.128.48.1"))
-#     
-#     # Adicionado em 18/04/2016 (medicoes de temperatura das cavidades de RF)
-#     nodes.append(Control_Node(48, 4, "r48n4", "10.128.48.4"))
-#     
-#     # Adicionado em 21/03/2016 (cavidades de RF do anel)
-#     nodes.append(Control_Node(48, 5, "r48n5", "10.128.48.5"))
-#     
-#     # Adicionado em 05/07/2016 (instalacao de fontes corretoras do Sirius no LINAC)
-#     nodes.append(Control_Node(49, 1, "r49n1", "10.128.49.1"))
-#     
-#     # Adicionado em 14/07/2016 (no com espiao da referencia externa da fonte A6QF01)
-#     nodes.append(Control_Node(49, 2, "r49n2", "10.128.49.2"))
+    #redes 36 e 38
+    nodes.append(Control_Node(36,1,'r36n1','10.128.1.127'))
+    nodes.append(Control_Node(38,2,'r38n2','10.128.1.120'))
+     
+    # rede 45
+    # 1 a 14
+    nodes.extend([Control_Node(45,i,'r45n%d'%i,'10.128.45.%d'%i) for i in range(1,15)])
+     
+    #rede 46
+    # 1 a 9, impares
+    nodes.extend([Control_Node(46,i,'r46n%d'%i,'10.128.46.%d'%i) for i in range(1,10,2)])
+    # 10 a 14
+    nodes.extend([Control_Node(46,i,'r46n%d'%i,'10.128.46.%d'%i) for i in range(10,15)])
+     
+    #nodes.append(Control_Node(45,9,'r45n9','10.128.45.9'))
+    #nodes.append(Control_Node(45,10,'r45n10','10.128.45.10'))
+    #nodes.append(Control_Node(45,11,'r45n11','10.128.45.11'))
+    #uvx_network.append(Control_Node(45,11,'r45n11','10.128.45.11'))
+     
+    # Adicionado em 21/03/2016 (cavidades de RF do anel)
+    nodes.append(Control_Node(48, 1, "r48n1", "10.128.48.1"))
+     
+    # Adicionado em 18/04/2016 (medicoes de temperatura das cavidades de RF)
+    nodes.append(Control_Node(48, 4, "r48n4", "10.128.48.4"))
+     
+    # Adicionado em 21/03/2016 (cavidades de RF do anel)
+    nodes.append(Control_Node(48, 5, "r48n5", "10.128.48.5"))
+     
+    # Adicionado em 05/07/2016 (instalacao de fontes corretoras do Sirius no LINAC)
+    nodes.append(Control_Node(49, 1, "r49n1", "10.128.49.1"))
+     
+    # Adicionado em 14/07/2016 (no com espiao da referencia externa da fonte A6QF01)
+    nodes.append(Control_Node(49, 2, "r49n2", "10.128.49.2"))
     
 
             
