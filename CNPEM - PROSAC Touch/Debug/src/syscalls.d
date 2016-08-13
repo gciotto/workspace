e4b3ae8d893887e2b92688a019ba6a0c11a1d164
gciotto <gustavociotto@gmail.com>
2016-08-13 19:51:37 -0300

    binary files deleted

--- CNPEM - PROSAC Touch/Debug/src/syscalls.d
+++
@@ -1,4 +0,0 @@
-src/syscalls.o: ../src/syscalls.c \
- /home/gciotto/workspace/workspace/CNPEM\ -\ PROSAC\ Touch/inc/mxconstants.h
-
-/home/gciotto/workspace/workspace/CNPEM\ -\ PROSAC\ Touch/inc/mxconstants.h:
--- CNPEM - PROSAC Touch/Debug/startup/startup_stm32f746xx.d
+++
@@ -1,4 +0,0 @@
-startup/startup_stm32f746xx.o: ../startup/startup_stm32f746xx.S \
- /home/gciotto/workspace/workspace/CNPEM\ -\ PROSAC\ Touch/inc/mxconstants.h
-
-/home/gciotto/workspace/workspace/CNPEM\ -\ PROSAC\ Touch/inc/mxconstants.h:
